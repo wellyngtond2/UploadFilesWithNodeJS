71b9282e1d8ce7ab98dd21cefdf06a591edd0e0f
wellyngtond2 <wellyngton_borges@hotmail.com>
2019-09-30 22:26:51 -0300

    add aws s3 connection

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,14 +1,32 @@
 const multer = require("multer");
 const path = require("path");
 const crypto = require("crypto");
-
-module.exports = {
-  dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
-  storage: multer.diskStorage({
+const multerS3 = require("multer-s3");
+const aws = require("aws-sdk");
+const storageTypes = {
+  local: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, path.resolve(__dirname, "..", "..", "tmp", "uploads"));
     },
     filename: (req, file, cb) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err);
+
+        file.key = `${hash.toString("hex")}-${file.originalname}`;
+
+        cb(null, file.key);
+      });
+    }
+  }),
+  s3: multerS3({
+    s3: new aws.S3({
+      endpoint: process.env.AWS_DEFAULT_REGION,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID
+    }),
+    bucket: "upload_example",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: "public-read",
+    key: (req, file, cb) => {
       crypto.randomBytes(16, (err, hash) => {
         if (err) cb(err);
 
@@ -17,7 +35,12 @@ module.exports = {
         cb(null, filename);
       });
     }
-  }),
+  })
+};
+
+module.exports = {
+  dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
+  storage: storageTypes["local"],
   limits: {
     fileSize: 2 * 1024 * 1024
   },
